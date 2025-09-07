f2174250b86c5a9e1e81746b3ee894170a4e425d
revemeili <reve@vt.edu>
2025-09-07 00:22:44 -0400

    Initial commit: add source and tests, ignore build artifacts

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/18048/workspace2/prime2
-# Build directory: /mnt/c/Users/18048/workspace2/prime2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(my_test "/mnt/c/Users/18048/workspace2/prime2/build/my_test")
-set_tests_properties(my_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/18048/workspace2/prime2/CMakeLists.txt;29;add_test;/mnt/c/Users/18048/workspace2/prime2/CMakeLists.txt;0;")
